aaa998024fba1dd90394a13cf9551a5c97c1935b
AlmuLloret <almudenalloret@gmail.com>
2023-03-21 14:57:49 +0100

    add template

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+/* SECCIÓN DE IMPORT */
+
+// - De React
+// - Nuestros
+// - Sass
+import '../styles/App.scss';
+// - Imágenes
+
+/* SECCIÓN DEL COMPONENTE */
+function App() {
+  /* VARIABLES ESTADO (DATOS) */
+
+  /* EFECTOS (código cuando carga la página) */
+
+  /* FUNCIONES HANDLER */
+
+  /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
+
+  /* HTML */
+  return(
+  <div className="App">
+
+
+  
+  </div>
+  ); 
+}
+
+/* PROP-TYPES */
+
+/* EXPORT DEL COMPONENTE */
+export default App;

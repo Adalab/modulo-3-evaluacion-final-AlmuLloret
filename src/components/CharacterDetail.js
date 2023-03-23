86261c629f119e9c78d1bf241061d2b6f08b738b
AlmuLloret <almudenalloret@gmail.com>
2023-03-23 21:37:54 +0100

    run docs

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,10 +1,10 @@
 import "../styles/Character.scss";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Gryffindor from "../../public/Gryffindor.jpg";
-import Slytherin from "../../public/Slytherin.jpg";
-import Hufflepuff from "../../public/Hufflepuff.jpg";
-import Ravenclaw from "../../public/Ravenclaw.jpg";
+import Gryffindor from "../images/Gryffindor.jpg";
+import Slytherin from "../images/Slytherin.jpg";
+import Hufflepuff from "../images/Hufflepuff.jpg";
+import Ravenclaw from "../images/Ravenclaw.jpg";
 import Error404 from "./Error404";
 
 function Character({ characterSelected }) {
@@ -27,27 +27,20 @@ function Character({ characterSelected }) {
         ? ""
         : `Patronus: ${characterSelected.patronus}`;
 
-    const houses = {
-      Gryffindor: Gryffindor,
-      Ravenclaw: Ravenclaw,
-      Slytherin: Slytherin,
-      Hufflepuff: Hufflepuff,
+    const getHouse = () => {
+      if (characterSelected.house === "Gryffindor") {
+        return Gryffindor;
+      }
+      if (characterSelected.house === "Ravenclaw") {
+        return Ravenclaw;
+      }
+      if (characterSelected.house === "Slytherin") {
+        return Slytherin;
+      }
+      if (characterSelected.house === "Hufflepuff") {
+        return Hufflepuff;
+      }
     };
-
-    // const getHouse = () => {
-    //   if (characterSelected.house === "Gryffindor") {
-    //     return Gryffindor;
-    //   }
-    //   if (characterSelected.house === "Ravenclaw") {
-    //     return Ravenclaw;
-    //   }
-    //   if (characterSelected.house === "Slytherin") {
-    //     return Slytherin;
-    //   }
-    //   if (characterSelected.house === "Hufflepuff") {
-    //     return Hufflepuff;
-    //   }
-    // };
 
     return (
       <article>
@@ -70,7 +63,7 @@ function Character({ characterSelected }) {
             <p className="card__text">{patronusHtml}</p>
             <img
               className="card__crest"
-              src={houses[characterSelected.house.toLowerCase()]}
+              src={getHouse()}
               alt="Pic"
               title="Pic"
             ></img>
